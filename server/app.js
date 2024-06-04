808f5310de24387da5b76664bc57835834c7af79
Ansh Verma <vermaansh708@gmail.com>
2024-06-04 17:18:50 +0530

    server static file

--- server/app.js
+++ server/app.js
@@ -3,6 +3,8 @@ import express from 'express';
 import homeRoute from './router/homeRoute.js';
 import dotenv from 'dotenv';
 import { connectDB } from './db.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 
@@ -12,13 +14,27 @@ const app = express();
 connectDB();
 
 const corsOptions = {
-    origin: '*', // Adjust the origin as needed
+    origin: '*', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
 };
 
 app.use(cors(corsOptions));
 app.use(express.json());
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+const clientPath = path.join(__dirname, '../client');
+app.use(express.static(clientPath));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(clientPath, 'index.html'));
+});
+
 app.use(homeRoute);
 
 app.listen(port, () => {
